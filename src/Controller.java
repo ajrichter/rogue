1d8f2b62f899b1da3ca2d981fd2ecd98134ea7d7
Myra Doubet <Mrdoubet@knox.edu>
2017-04-10 12:40:52 -0500

    Beginning of using space bar for narration

--- src/Controller.java
+++ src/Controller.java
@@ -13,6 +13,7 @@ public class Controller extends JFrame implements KeyListener {
     private GamePlay game;
     private RougeView view;
     public static JFrame frame;
+    public boolean narration;
     
     public Controller(String s) {
         super(s);
@@ -25,6 +26,7 @@ public class Controller extends JFrame implements KeyListener {
         setVisible(true);
         */
         
+        narration = false;
         frame= new JFrame();
 		frame.setVisible(true);
 		Graphics gr = frame.getGraphics();
@@ -42,84 +44,90 @@ public class Controller extends JFrame implements KeyListener {
 	public void keyPressed (KeyEvent e) {
 		int key = e.getKeyCode();
 		
+		if (key == KeyEvent.VK_SPACE) {
+			narration = false;
+			view.updateNaration("");
+		}
 		
-		if (key == KeyEvent.VK_SLASH) {
-	        //Tells what the next character you type is
-	    }
+		if (narration == false) {
+			if (key == KeyEvent.VK_SLASH) {
+				//Tells what the next character you type is
+			}
 		
-		if (key == KeyEvent.VK_SLASH) {
-	        //Tells what the next character you type is
-	    }
+			if (key == KeyEvent.VK_SLASH) {
+				//Tells what the next character you type is
+			}
 		
-	    if (key == KeyEvent.VK_H) {
-	    	int[] left = {-1,0};
-	    	System.out.println("left");
-	    	game.move(left);
-	    }         
+			if (key == KeyEvent.VK_H) {
+				int[] left = {-1,0};
+				System.out.println("left");
+				narration = game.move(left);
+			}         
 	
-	    if (key == KeyEvent.VK_L) {
-	        int[] right = {1,0};
-	        System.out.println("right");
-	        game.move(right);
-	    }
+			if (key == KeyEvent.VK_L) {
+				int[] right = {1,0};
+				System.out.println("right");
+				narration = game.move(right);
+			}
 	
-	    if (key == KeyEvent.VK_J) {
-	        int[] up = {0,1};
-	        System.out.println("up");
-	        game.move(up);
-	    }
+			if (key == KeyEvent.VK_J) {
+				int[] up = {0,1};
+				System.out.println("up");
+				narration = game.move(up);
+			}
 	
-	    if (key == KeyEvent.VK_K) {
-	        int[] down = {0,-1};
-	        System.out.println("down");
-	        game.move(down);
-	    }
+			if (key == KeyEvent.VK_K) {
+				int[] down = {0,-1};
+				System.out.println("down");
+				narration = game.move(down);
+			}
 	    
-	    if (key == KeyEvent.VK_Y) {
-	    	//moves diagonally left and up
-	    	int[] leftup = {-1,1};
-	    	System.out.println("leftup");
-	    	game.move(leftup);
-	    }         
+			if (key == KeyEvent.VK_Y) {
+				//moves diagonally left and up
+				int[] leftup = {-1,1};
+				System.out.println("leftup");
+				narration = game.move(leftup);
+			}         
 	
-	    if (key == KeyEvent.VK_U) {
-	    	//moves diagonally right and up
-	        int[] rightup = {1,1};
-	        System.out.println("rightup");
-	        game.move(rightup);
-	    }
+			if (key == KeyEvent.VK_U) {
+				//moves diagonally right and up
+				int[] rightup = {1,1};
+				System.out.println("rightup");
+				narration = game.move(rightup);
+			}
 	
-	    if (key == KeyEvent.VK_B) {
-	    	//moves diagonally left and down
-	        int[] leftdown = {-1,-1};
-	        System.out.println("leftdown");
-	        game.move(leftdown);
-	    }
+			if (key == KeyEvent.VK_B) {
+				//moves diagonally left and down
+				int[] leftdown = {-1,-1};
+				System.out.println("leftdown");
+				narration = game.move(leftdown);
+			}
 	
-	    if (key == KeyEvent.VK_N) {
-	    	//moves diagonally right and down
-	        int[] rightdown = {1,-1};
-	        System.out.println("rightdown");
-	        game.move(rightdown);
-	    }
+			if (key == KeyEvent.VK_N) {
+				//moves diagonally right and down
+				int[] rightdown = {1,-1};
+				System.out.println("rightdown");
+				narration = game.move(rightdown);
+			}
 	    
-	    if (key == KeyEvent.VK_T) {
-	        //Throws an object in the next specified direction
-	    }
+			if (key == KeyEvent.VK_T) {
+				//Throws an object in the next specified direction
+			}
 	    
-	    if (key == KeyEvent.VK_F) {
-	        //Fights until someone dies
-	    	//Given a direction, fight the enemy in that direction
-	    }
+			if (key == KeyEvent.VK_F) {
+				//Fights until someone dies
+				//Given a direction, fight the enemy in that direction
+			}
 	    
-	    if (key == KeyEvent.VK_M) {
-	        //Move onto something without picking it up
-	    	//must be given a direction after and only moves one step without picking it up
-	    }
+			if (key == KeyEvent.VK_M) {
+				//Move onto something without picking it up
+				//must be given a direction after and only moves one step without picking it up
+			}
 	    
-	    if (key == KeyEvent.VK_Z) {
-	        //uses a wand or stave, but must be pointed in a direction
-	    }
+			if (key == KeyEvent.VK_Z) {
+				//uses a wand or stave, but must be pointed in a direction
+			}
+		}
 	    
 	}
 	
