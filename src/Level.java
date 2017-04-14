f7fc22563673b7860454130abab4795f63c14623
ajrichter <ajrichter@knox.edu>
2017-04-14 15:23:06 -0500

    cleaning level...

--- src/Level.java
+++ src/Level.java
@@ -1,4 +1,3 @@
-import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
 import Item.Item;
 import java.awt.Point;
@@ -12,31 +11,32 @@ import java.util.ArrayList;
  * 
  */
 public class Level {
-	private String[][] floorSeen;
-	private Unit[][] floorUnits;
-	private Item[][] floorItems;
-	private String[][] floorTraps;
-	private Room[] rooms;
-
-	// Andrew's  Instance Variables
 	private int numLevel;
 	// Number of Rooms
 	private int numR;
 	// The full floor
 	private char[][] floor;
-	// if you've seen the square
+	// Visible part of the floor
 	private boolean[][] isSeen;
-	// 0-8 of rooms if it is a room
+	// Array of Rooms Existence
 	private boolean[] rb;
-	// The Rms if they exist
+	// Array of Rooms
 	private Rm[] rs;
-	
-	//list of enemy
+	// List of enemies
 	private ArrayList<Enemy> enList;
+	
+	// Need something similar for Items?
+	// Ink is right. Make a Room class. But simple!!
+	// door, edges, items, 
+	// How do I do hallways then?
+	// Things to think about...
+	
 	//list of available enemies
 	private String[] allEnemy;
 	//position of stair, 2 elements, stair[0] = x, stair[1] = y
 	protected int[] stair;
+	
+	// Player needs to be in GamePlay too though
 	protected Player p;
 	
 	// An inner class used to define each room
@@ -85,19 +85,6 @@ public class Level {
 		this.enList = new ArrayList<Enemy>();
 		// Great I just do not understand this part
 		this.allEnemy = new String[] {"A","B","C","D","E","F"};
-	}
-
-	public Level(Unit player){
-		makeLevel();
-
-		floorUnits= new Unit[24][80];
-		floorItems= new Item[24][80];
-
-		rooms = new Room[9];	//9 possible rooms
-
-		Enemy e= new Enemy();
-		addUnit(player);
-		addUnit(e);
 	}
 	
 	private void placeItem(int x, int y){
@@ -234,24 +221,6 @@ public class Level {
 		return r;
 	}
 
-	// Delete
-	//Small for prototype
-	// Yes, I can tell
-	public void makeLevel(){
-		this.floorSeen= new String[20][20];
-		for(int i=0; i<20;i++){
-			for(int k=0; k<20;k++){
-				if(i==0 || i==19){
-					this.floorSeen[i][k]= "=";
-				}else if(k==0 ||k==19){
-					this.floorSeen[i][k]="|";
-				}else{
-					this.floorSeen[i][k]=".";
-				}
-			}
-		}
-	}
-
 	public boolean[][] getSeen(){
 		return isSeen;
 	}
@@ -271,158 +240,37 @@ public class Level {
 		return pfloor;
 	}
 
-	public String[][] getSeenFloor(){
-		return floorSeen;
-	}
-
-	//adds a unit to the level
-	private void addUnit(Unit u){
-
-		Random rand = new Random();
-		int  tempX = rand.nextInt(this.floorSeen.length);
-		int tempY = rand.nextInt(this.floorSeen[0].length);
-
-		//look for empty space on a floor
-		while(this.floorSeen[tempX][tempY]!="."){
-			tempX = rand.nextInt(this.floorSeen.length);
-			tempY = rand.nextInt(this.floorSeen[0].length);
-		}
-		this.floorUnits[tempX][tempY]=u;
-		this.floorSeen[tempX][tempY]=u.toString();
-	}
-
 	//checks if a move is valid or not, this method is working
 	public boolean validMove(Unit u, int[] dir){
-		// int[] location= unitLocation(u);
+		// inside a room || door || hall
+		// && No out of bounds check!
+		// Else no move
 		
 		Point a = p.getP();
 
 		if(floor[a.y + dir[1]][a.y + dir[0]]  == '.'){
 			return true;
 		}
-		
-		//if out of bound
-		/*
-		if(location[0]+dir[0]<0 ||
-		   location[0]+dir[0]>(floorUnits.length-1)||
-		   location[1]+dir[1]<0 ||
-		   location[1]+dir[1]>(floorUnits[0].length-1)){
-			return false;
-		}else{
-			//check if piece of floor
-			if(floorSeen[location[1]+dir[1]][location[0]+dir[0]].equals(".")||
-			   floorSeen[location[1]+dir[1]][location[0]+dir[0]].equals("A")){
-				return true;
-			}
-		}*/
-
 		return false;
 	}
 
-	//if player hits enemy
-	public Enemy isEnemy(Unit u, int[] dir){
-		int[] location= unitLocation(u);
-
-		//if out of bound
-		if(location[0]+dir[0]<0 ||
-		   location[0]+dir[0]>(floorUnits.length-1)||
-		   location[1]+dir[1]<0 ||
-		   location[1]+dir[1]>(floorUnits[0].length-1)){
-			return null;
-		}else{
-			//check if piece of floor
-			if(Character.isLetter(floorSeen[location[1]+dir[1]][location[0]+dir[0]].charAt(0))){
-				return (Enemy) floorUnits[location[1]+dir[1]][location[0]+dir[0]];
-			}
-		}
-
-		return null;
-	}
-
-	//if player hits enemy
-		public Item isItem(Unit u, int[] dir){
-			int[] location= unitLocation(u);
-
-			//if out of bound
-			if(location[0]+dir[0]<0 ||
-			   location[0]+dir[0]>(floorUnits.length-1)||
-			   location[1]+dir[1]<0 ||
-			   location[1]+dir[1]>(floorUnits[0].length-1)){
-				return null;
-			}else{
-				//check if piece of floor
-				if(floorSeen[location[1]+dir[1]][location[0]+dir[0]].equals("A")){
-					return (Item) floorItems[location[1]+dir[1]][location[0]+dir[0]];
-				}
-			}
-
-			return null;
-		}
-		
-	// No. This should be stored by Unit as a Point
-	// when the Enemy is created initially 
-	public int[] unitLocation(Unit u){
-		int[] temp= new int[2];
-
-		for(int i=0; i<floorUnits.length;i++){
-			for(int k=0; k<floorUnits.length;k++){
-				if(u.equals(floorUnits[i][k])){
-					temp[0]=k;
-					temp[1]=i;
-				}
-			}
-		}
-		return temp;
-	}
-	// Maybe this should be move a point in a certain direction or
-	// the unit at the point
-	//moves the player in a certain direction
 	public boolean moveUnit(Unit u, int[] dir){
-		// int[] location= unitLocation(u);
-		
 		Point a = p.getP();
+		// if(validMove(...
+			
+		floor[(int) a.getY()][(int) a.getX()] = '.';
+		p.setP((int) a.getX() + dir[0], (int) a.getY() + dir[1]);
+		floor[(int) a.getY() + dir[1]][(int) a.getX() + dir[0]] = '@';
 		
-		// if(validMove(u, dir)){
-			
-			floor[(int) a.getY()][(int) a.getX()] = '.';
-			p.setP((int) a.getX() + dir[0], (int) a.getY() + dir[1]);
-			floor[(int) a.getY() + dir[1]][(int) a.getX() + dir[0]] = '@';
-			
-			//make previous location clear
-//			if(floorItems[location[1]][location[0]]==null){
-				// floorSeen[location[1]][location[0]]= ".";
-			//}else{
-			//	floorSeen[location[1]][location[0]]=floorItems[location[1]][location[0]].toString();
-			//}
-			// floorUnits[location[1]][location[0]]= null;
-			//add units to current location
-			//floorSeen[(location[1]+dir[1])][(location[0]+dir[0])]=u.toString();
-			//floorUnits[(location[1]+dir[1])][(location[0]+dir[0])]= u;
-			return true;
-		//}
-
-		//return false;
+		return true;
 	}
 
-	//called by enemy who dies
+	// remove enemy
 	public void removeUnit(Unit u){
-		int[] loc= unitLocation(u);
-		this.floorUnits[loc[1]][loc[0]]=null;
-		if(this.floorItems[loc[1]][loc[0]]==null){
-			this.floorSeen[loc[1]][loc[0]]=".";
-		}
 	}
 
 	//picks up an item
 	public void pickUp(Unit u, int[] direction){
-		int[] loc= unitLocation(u);
-		this.floorItems[loc[1]+direction[1]][loc[0]+direction[0]]=null;
-		this.floorSeen[loc[1]+direction[1]][loc[0]+direction[0]]=".";
-	}
-
-	public void addItem(Item item, int[] location){
-		floorItems[location[1]][location[0]]=item;
-		floorSeen[location[1]][location[0]]=item.toString();
 	}
 
 	/**
